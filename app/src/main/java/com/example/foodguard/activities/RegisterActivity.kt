c3d837f9d38e3923ecabf37c48693aab9fa31cf6
nitzan <nitzan.h.2000@gmail.com>
2025-01-23 02:26:35 +0200

    create post deateiles and list

--- app/src/main/java/com/example/foodguard/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/foodguard/activities/RegisterActivity.kt
@@ -1,10 +1,11 @@
-package com.example.foodguard
+package com.example.foodguard.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foodguard.R
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
